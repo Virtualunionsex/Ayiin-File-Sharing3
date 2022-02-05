5366e7265bcbcd55fb5dde02b2ddcd014dc924a7
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-05 14:53:52 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -126,7 +126,6 @@ async def start_command(client: Client, message: Message):
                 InlineKeyboardButton("•Tᴜᴛᴜᴘ", callback_data="close"),
             ],
         ]
-        await message.reply_text(
             text=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
