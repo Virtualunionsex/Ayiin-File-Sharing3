2ffb1c51cc02b001af26af81cce64eb03b199925
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-06-11 19:01:27 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/AyiinXdSupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/nakama_asl untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_GROUP:
@@ -97,13 +97,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/AyiinXdSupport untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/nakama_asl untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/AyiinXdSupport"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/nakama_asl"
         )
         self.username = usr_bot_me.username
 
